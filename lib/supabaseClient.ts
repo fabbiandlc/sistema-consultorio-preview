cc9d47097507f0c23a1ca8253d6d004beb7b6346
fabiansking <fahuervodelacruz@hotmail.com>
2025-07-01 14:31:52 -0600

    Deploy

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mpffnwrysmtmuerwtfhh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wZmZud3J5c210bXVlcnd0ZmhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyMjc3NzIsImV4cCI6MjA2NjgwMzc3Mn0.fHaZp2ngDRVkTdiuqTn6Njnl1mVboZKCaXE0Lh6QGyc'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseKey) 
